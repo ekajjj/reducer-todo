8e54a9fb5d5d17e12831172bccd2d5a88d9d8eaa
Jake Johnson <jakejjohnson@gmail.com>
2020-03-16 16:04:59 -0500

    .

--- reducer-todo/src/components/TodoComponent.js
+++ reducer-todo/src/components/TodoComponent.js
@@ -4,7 +4,7 @@ const Todo = (props) => {
     const { id, toggleComplete, item, completed } = props;
     return (
         <li onClick={() => toggleComplete(id)} style={{ textDecoration: completed ? 'line-through' : 'none' }}>
-            {item} ({id})
+            {item} (cuid: {id})
 
         </li>
     )
